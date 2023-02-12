d4c7ae592727898a6cb6baff6efce7e66512041f
The-Minecraft-Scientist <The-Minecraft-Scientist@users.noreply.github.com>
2023-02-11 22:55:31 -0800

    Initial Commit

---
+++ ChaosSaver/ChaosSaverView.h
@@ -0,0 +1,12 @@
+//
+//  ChaosSaverView.h
+//  ChaosSaver
+//
+//  Created by Charles Liske on 2/11/23.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ChaosSaverView : ScreenSaverView
+
+@end
---
+++ ChaosSaver/ChaosSaverView.m
@@ -0,0 +1,51 @@
+//
+//  ChaosSaverView.m
+//  ChaosSaver
+//
+//  Created by Charles Liske on 2/11/23.
+//
+
+#import "ChaosSaverView.h"
+
+@implementation ChaosSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
